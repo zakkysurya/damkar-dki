51e99ed6edfdd2023bd6d601c8660a112cb39732
Zakky <zakkysurya@gmail.com>
2025-05-22 20:38:06 +0700

    added detail report project

--- resources/views/report_project/detail.blade.php
+++ resources/views/report_project/detail.blade.php
@@ -2,121 +2,101 @@
 @section('main-content')
 <div class="row">
 
-      <div class="col-lg-4 order-lg-2">
+      <div class="col-lg-12 order-lg-1">
+
 
             <div class="card shadow mb-4">
-                  <div class="card-profile-image mt-4">
-                        <figure class="rounded-circle avatar avatar font-weight-bold" style="font-size: 60px; height: 180px; width: 180px;" data-initial="{{ Auth::user()->name[0] }}"></figure>
-                  </div>
-                  <div class="card-body">
-
-                        <div class="row">
-                              <div class="col-lg-12">
-                                    <div class="text-center">
-                                          <h5 class="font-weight-bold">{{ Auth::user()->fullName }}</h5>
-                                          <p>Administrator</p>
-                                    </div>
-                              </div>
-                        </div>
-
-                        <div class="row">
-                              <div class="col-md-4">
-                                    <div class="card-profile-stats">
-                                          <span class="heading">22</span>
-                                          <span class="description">Friends</span>
-                                    </div>
-                              </div>
-                              <div class="col-md-4">
-                                    <div class="card-profile-stats">
-                                          <span class="heading">10</span>
-                                          <span class="description">Photos</span>
-                                    </div>
-                              </div>
-                              <div class="col-md-4">
-                                    <div class="card-profile-stats">
-                                          <span class="heading">89</span>
-                                          <span class="description">Comments</span>
-                                    </div>
-                              </div>
-                        </div>
-                  </div>
-            </div>
-
-      </div>
-
-      <div class="col-lg-8 order-lg-1">
-
-            <div class="card shadow mb-4">
-
                   <div class="card-header py-3">
-                        <h6 class="m-0 font-weight-bold text-primary">My Account</h6>
+                        <h6 class="m-0 font-weight-bold text-primary">Project Detail</h6>
                   </div>
-
                   <div class="card-body">
-
                         <form method="POST" action="" autocomplete="off">
-                              <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                              <h6 class="heading-small text-muted mb-4">Man Power</h6>
+                              <div class="pl-lg-4">
+                                    <div class="row">
+                                          <div class="col-lg-6">
+                                                <div class="form-group focused">
+                                                      <label class="form-control-label" for="name">Nama Lengkap</label>
+                                                      <input type="text" class="form-control disabled" value="{{ $manPower->nama_lengkap }}" readonly>
+                                                </div>
+                                          </div>
+                                          <div class="col-lg-6">
+                                                <div class="form-group focused">
+                                                      <label class="form-control-label" for="name">Jabatan</label>
+                                                      <input type="text" class="form-control disabled" value="{{ $manPower->jabatan }}" readonly>
+                                                </div>
+                                          </div>
+                                    </div>
+                              </div>
 
-                              <input type="hidden" name="_method" value="PUT">
-
-                              <h6 class="heading-small text-muted mb-4">User information</h6>
+                              <h6 class="heading-small text-muted mb-4">Project</h6>
+                              <div class="pl-lg-4">
+                                    <div class="row">
+                                          <div class="col-lg-6">
+                                                <div class="form-group focused">
+                                                      <label class="form-control-label" for="name">Name</label>
+                                                      <input type="text" class="form-control disabled" value="{{ $project->nama_project }}" readonly>
+                                                </div>
+                                          </div>
+                                          <div class="col-lg-6">
+                                                <div class="form-group focused">
+                                                      <label class="form-control-label" for="name">Deskripsi</label>
+                                                      <input type="text" class="form-control disabled" value="{{ $project->deskripsi }}" readonly>
+                                                </div>
+                                          </div>
+                                    </div>
+                              </div>
 
                               <div class="pl-lg-4">
                                     <div class="row">
                                           <div class="col-lg-6">
                                                 <div class="form-group focused">
-                                                      <label class="form-control-label" for="name">Name<span class="small text-danger">*</span></label>
-                                                      <input type="text" id="name" class="form-control" name="name" placeholder="Name" value="{{ old('name', Auth::user()->name) }}">
+                                                      <label class="form-control-label" for="name">Tanggal Mulai</label>
+                                                      <input type="text" class="form-control disabled" value="{{ $project->tanggal_mulai }}" readonly>
                                                 </div>
                                           </div>
                                           <div class="col-lg-6">
                                                 <div class="form-group focused">
-                                                      <label class="form-control-label" for="last_name">Last name</label>
-                                                      <input type="text" id="last_name" class="form-control" name="last_name" placeholder="Last name" value="{{ old('last_name', Auth::user()->last_name) }}">
-                                                </div>
-                                          </div>
-                                    </div>
-
-                                    <div class="row">
-                                          <div class="col-lg-12">
-                                                <div class="form-group">
-                                                      <label class="form-control-label" for="email">Email address<span class="small text-danger">*</span></label>
-                                                      <input type="email" id="email" class="form-control" name="email" placeholder="example@example.com" value="{{ old('email', Auth::user()->email) }}">
-                                                </div>
-                                          </div>
-                                    </div>
-
-                                    <div class="row">
-                                          <div class="col-lg-4">
-                                                <div class="form-group focused">
-                                                      <label class="form-control-label" for="current_password">Current password</label>
-                                                      <input type="password" id="current_password" class="form-control" name="current_password" placeholder="Current password">
-                                                </div>
-                                          </div>
-                                          <div class="col-lg-4">
-                                                <div class="form-group focused">
-                                                      <label class="form-control-label" for="new_password">New password</label>
-                                                      <input type="password" id="new_password" class="form-control" name="new_password" placeholder="New password">
-                                                </div>
-                                          </div>
-                                          <div class="col-lg-4">
-                                                <div class="form-group focused">
-                                                      <label class="form-control-label" for="confirm_password">Confirm password</label>
-                                                      <input type="password" id="confirm_password" class="form-control" name="password_confirmation" placeholder="Confirm password">
+                                                      <label class="form-control-label" for="name">Tanggal Selesai</label>
+                                                      <input type="text" class="form-control disabled" value="{{ $project->tanggal_selesai }}" readonly>
                                                 </div>
                                           </div>
                                     </div>
                               </div>
+
+
+                              <h6 class="heading-small text-muted mb-4">Tasks</h6>
+                              <div class="pl-lg-4">
+                                    @foreach($tasks as $task)
+                                    <div class="row">
+                                          <div class="col-lg-6">
+                                                <div class="form-group focused">
+                                                      <label class="form-control-label" for="name">Judul</label>
+                                                      <input type="text" class="form-control disabled" value="{{ $task->judul_task }}" readonly>
+                                                </div>
+                                          </div>
+                                          <div class="col-lg-6">
+                                                <div class="form-group focused">
+                                                      <label class="form-control-label" for="name">Status</label>
+                                                      <input type="text" class="form-control disabled" value="{{ $task->status }}" readonly>
+                                                </div>
+                                          </div>
+                                    </div>
+                                    @endforeach
+                              </div>
+
 
                               <!-- Button -->
                               <div class="pl-lg-4">
                                     <div class="row">
-                                          <div class="col text-center">
-                                                <button type="submit" class="btn btn-primary">Save Changes</button>
+                                          <div class="col">
+                                                <a type="submit" href="{{ route('report-project.index') }}" class="btn btn-primary">Back</a>
                                           </div>
                                     </div>
                               </div>
                         </form>
+
+
 
                   </div>
 
